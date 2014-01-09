406dd1ddd9e5e85b8f999650be5e2f6a205f3c66
anil <vishnupriyaanil@gmail.com>
2014-01-09 16:21:36 +0530

    Revert "anil kumar"
    This reverts commit 6e9fb9949d75d8e2e4fb423ce15cbe70ddaadb3c.

--- src/com/exmple/anil/MainActivity.java
+++ src/com/exmple/anil/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		System.out.println("Anil Kumar GN");
 	}
 
 	@Override
