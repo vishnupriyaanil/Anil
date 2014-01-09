2af80e3adc239896519bfc5ddd48d32de18bec1b
anil <vishnupriyaanil@gmail.com>
2014-01-09 16:28:10 +0530

    again commit in main activity
    again commit in main activity

--- src/com/exmple/anil/MainActivity.java
+++ src/com/exmple/anil/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		System.out.println("Anil Kumar GN");
+		System.out.println("Anil Kumar GN");
 	}
 
 	@Override
